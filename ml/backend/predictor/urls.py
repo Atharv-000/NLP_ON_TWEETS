1f5edcd723c001f16688c4f87c283092ee71532a
Atharv Sharma <omprakash26101948@gmail.com>
2025-12-20 01:56:58 +0530

    Update urls.py

--- ml/backend/predictor/urls.py
+++ ml/backend/predictor/urls.py
@@ -1,15 +1,8 @@
 from django.urls import path
-from .views import predict_api, predict_csv_api
-from django.urls import path
 from .views import home, predict_api, predict_csv_api
 
 urlpatterns = [
-    path('predict/', predict_api),
-    path('predict-csv/', predict_csv_api),
-]
-
-urlpatterns = [
-    path("", home),  # 👈 ROOT PATH
+    path("", home),                    # /
     path("api/predict/", predict_api),
     path("api/predict-csv/", predict_csv_api),
 ]
